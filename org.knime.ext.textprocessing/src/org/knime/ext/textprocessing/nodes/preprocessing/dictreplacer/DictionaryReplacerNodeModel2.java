567a2151bfba35b408964fe6318521038ec49540
Andisa Dewi <andisa.dewi@icloud.com>
2017-11-29 16:46:27 +0100

    AP-8315: Dict Replacer now supports reading from knime protocol
    AP-8315 (Text Mining: IO nodes to support knime protocol)

--- org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/preprocessing/dictreplacer/DictionaryReplacerNodeModel2.java
+++ org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/preprocessing/dictreplacer/DictionaryReplacerNodeModel2.java
@@ -49,10 +49,10 @@
 package org.knime.ext.textprocessing.nodes.preprocessing.dictreplacer;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -63,8 +63,10 @@ import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.defaultnodesettings.SettingsModelOptionalString;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.util.FileUtil;
 import org.knime.ext.textprocessing.nodes.preprocessing.StreamableFunctionPreprocessingNodeModel;
 import org.knime.ext.textprocessing.nodes.preprocessing.TermPreprocessing;
+import org.knime.ext.textprocessing.nodes.source.parser.FileCollector2;
 import org.knime.ext.textprocessing.nodes.tokenization.MissingTokenizerException;
 import org.knime.ext.textprocessing.nodes.tokenization.TokenizerFactoryRegistry;
 import org.knime.ext.textprocessing.util.DataTableSpecVerifier;
@@ -141,6 +143,8 @@ public final class DictionaryReplacerNodeModel2 extends StreamableFunctionPrepro
         if (!dataTableSpecVerifier.verifyTokenizer(m_tokenizerModel.getStringValue())) {
             setWarningMessage(dataTableSpecVerifier.getTokenizerWarningMsg());
         }
+        // check file path
+        FileCollector2.getURL(m_fileModel.getStringValue(), false);
     }
 
     /**
